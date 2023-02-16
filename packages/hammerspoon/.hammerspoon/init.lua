591178916d5c89627cf960fa5cb1953ef9d64aea
yosukenakaguchi <yosuke.nakaguchi.private@gmail.com>
2023-02-16 21:42:47 +0900

    ✨ Add hammerspoon

---
+++ packages/hammerspoon/.hammerspoon/init.lua
@@ -0,0 +1,8 @@
+hs.hotkey.bind({"alt"}, "space", function()
+  local alacritty = hs.application.find('alacritty')
+  if alacritty:isFrontmost() then
+    alacritty:hide()
+  else
+    hs.application.launchOrFocus("/Applications/Alacritty.app")
+  end
+end)
